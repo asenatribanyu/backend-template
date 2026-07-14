9ef040361a3f3c42d622015f7c50c7678399048f
asenatribanyu <asenatribanyu@gmail.com>
2026-07-14 11:27:25 +0700

    chore: setup eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier/recommended";
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        process: "readonly",
+        console: "readonly",
+        Buffer: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        setTimeout: "readonly",
+        setInterval: "readonly",
+        clearTimeout: "readonly",
+        clearInterval: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "prettier/prettier": "error",
+    },
+    ignores: ["node_modules/**", "coverage/**", "logs/**", "storage/**"],
+  },
+];
